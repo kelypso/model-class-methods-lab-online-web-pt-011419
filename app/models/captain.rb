2bcac80b52e0cedaeb288ea9f2c2b5002d1d7e8c
Kelsey Low <kklowlow@gmail.com>
2019-07-06 04:38:05 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,23 +2,30 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    # includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+    # return all catamaran captains by finding boats that include a
+    # classification where the classification is catamaran
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors
-    # includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+    # return all unique sailboat captains (sailors) by finding boats that
+    # include a classification where the classification is sailboat
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
   end
 
   def self.motorboat_operators
-    # includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    #
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
   end
 
   def self.talented_seafarers
-    # where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+    #
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
   def self.non_sailors
-    # where.not("id IN (?)", self.sailors.pluck(:id))
+    #
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
 
 end
